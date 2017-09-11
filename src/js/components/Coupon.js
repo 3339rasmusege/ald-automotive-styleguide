5c803f302bd303ca5206947e16c508bf1bb5e816
Jacques Mourier <jacquesmourier@gmail.com>
2017-09-11 21:08:49 +0200

    Coupon

--- src/js/components/Coupon.js
+++ src/js/components/Coupon.js
@@ -1,2 +1,27 @@
-// Coupon
+( function() {
 
+  var coupon = $('.Coupon')
+  var couponField = coupon.find('.Coupon-field')
+  var couponButton = coupon.find('.Coupon-button')
+
+  function showButton() {
+    if( couponField.val() ) {
+      coupon.addClass('is-open')
+      coupon.css({ 'height': '160px' })
+      setTimeout( function() {
+        couponButton.css({ 'opacity': '1' })
+      }, 200)
+    } else {
+      couponButton.css({ 'opacity': '0' })
+      setTimeout( function() {
+        coupon.removeClass('is-open')
+        coupon.css({ 'height': '98px' })
+      }, 200)
+    }
+  }
+
+  couponField.keyup( function() {
+    showButton()
+  })
+
+})();
